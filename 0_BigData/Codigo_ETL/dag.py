0a992bb1579a4c1d6263675209989913a51ef089
inesposes <inesposes@gmail.com>
2025-06-10 21:30:27 +0200

    Created dag for pipeline

---
+++ 0_BigData/Codigo_ETL/dag.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+default_args = {
+    'start_date': datetime(2025, 6, 9),
+    'retries': 1
+}
+
+with DAG('etl-pipeline', 
+         default_args=default_args, 
+         schedule_interval='@monthly', 
+         catchup=False) as dag:
+
+    # run_data_quality = BashOperator(
+    #     task_id='run_data_quality',
+    #     bash_command='gcloud dataproc jobs submit pyspark gs://naturgy-gcs/scripts/data_quality.py --cluster=naturgy-spark --region=europe-southwest1 --py-files gs://naturgy-gcs/scripts/logger_config.py'
+    # )
+
+    run_silver = BashOperator(
+        task_id='run_silver',
+        bash_command='gcloud dataproc jobs submit pyspark gs://naturgy-gcs/scripts/silver.py --cluster=naturgy-spark --region=europe-southwest1 --py-files gs://naturgy-gcs/scripts/data_quality.py,gs://naturgy-gcs/scripts/logger_config.py'
+    )
+
+    run_gold = BashOperator(
+        task_id='run_gold',
+        bash_command='gcloud dataproc jobs submit pyspark gs://naturgy-gcs/scripts/gold.py --cluster=naturgy-spark --region=europe-southwest1 --py-files gs://naturgy-gcs/scripts/logger_config.py --files gs://naturgy-gcs/scripts/external_data.json'
+    )
+
+    run_silver >> run_gold
