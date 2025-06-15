7099dd9f547e5c4f42180258b6582610aec18e56
inesposes <inesposes@gmail.com>
2025-06-15 10:51:00 +0200

    data quality correxido

--- 0_BigData/Codigo_ETL/ETL_produccion/data_quality.py
+++ 0_BigData/Codigo_ETL/ETL_produccion/data_quality.py
@@ -24,16 +24,15 @@ def count_duplicates(df, target_columns, report_data):
     except Exception as e:
         logger.error(f"Error en count_duplicates: {e}")
 
-def count_nulls(df, report_data):
+def count_nulls(df, report_data, include_columns=None):
     if not df:
         logger.warning("DataFrame de entrada para count_non_nulls es None. Saltando paso.")
         return
     try:
         logger.info("Iniciando recuento de valores no nulos por columna.")
-        
+        analysis_columns = include_columns if include_columns else df.columns
         total_rows = df.count()
-        
-        null_counts_df = df.select([spark_sum(col(c).isNull().cast('int')).alias(c) for c in df.columns])
+        null_counts_df = df.select([spark_sum(col(c).isNull().cast('int')).alias(c) for c in analysis_columns]) 
         null_counts_dict = null_counts_df.collect()[0].asDict()
         non_null_counts_dict = {col: total_rows - nulls for col, nulls in null_counts_dict.items()}
         logger.info(f"Recuento de valores no nulos: {non_null_counts_dict}")
@@ -50,19 +49,19 @@ def weather_quality_rules(df, report_data):
     try:
         logger.info("Aplicando reglas de calidad de datos al dataset del clima.")
         quality_checks = OrderedDict([
-            ("temp", df.filter((col("temp") >= 238.15) & (col("temp") <= 321.15)).count()),
-            ("temp_min", df.filter((col("temp_min") >= 238.15) & (col("temp_min") <= 321.15)).count()),
-            ("temp_max", df.filter((col("temp_max") >= 238.15) & (col("temp_max") <= 321.15)).count()),
+            ("temp_range", df.filter((col("temp") >= 238.15) & (col("temp") <= 321.15)).count()),
+            ("temp_min_range", df.filter((col("temp_min") >= 238.15) & (col("temp_min") <= 321.15)).count()),
+            ("temp_max_range", df.filter((col("temp_max") >= 238.15) & (col("temp_max") <= 321.15)).count()),
             ("ud_celsius", df.filter((col("temp") >= -50) & (col("temp") <= 60)).count()),
-            ("pressure", df.filter((col("pressure") >= 800) & (col("pressure") <= 1100)).count()),
-            ("wind_speed", df.filter((col("wind_speed") >= 0) & (col("wind_speed") <= 68.88)).count()),
+            ("pressure_range", df.filter((col("pressure") >= 800) & (col("pressure") <= 1100)).count()),
+            ("wind_speed_range", df.filter((col("wind_speed") >= 0) & (col("wind_speed") <= 68.88)).count()),
             ("ud_kmh", df.count() if df.select(avg("wind_speed")).first()[0] > 7 else 0),
-            ("wind_deg", df.filter((col("wind_deg") >= 0) & (col("wind_deg") <= 360)).count()),
-            ("rain_1h", df.filter(col("rain_1h") >= 0).count()),
-            ("rain_3h", df.filter(col("rain_3h") >= 0).count()),
-            ("snow_3h", df.filter(col("snow_3h") >= 0).count()),
-            ("humidity", df.filter((col("humidity") >= 0) & (col("humidity") <= 100)).count()),
-            ("clouds_all", df.filter((col("clouds_all") >= 0) & (col("clouds_all") <= 100)).count()),
+            ("wind_deg_range", df.filter((col("wind_deg") >= 0) & (col("wind_deg") <= 360)).count()),
+            ("rain_1h_positive", df.filter(col("rain_1h") >= 0).count()),
+            ("rain_3h_positive", df.filter(col("rain_3h") >= 0).count()),
+            ("snow_3h_positive", df.filter(col("snow_3h") >= 0).count()),
+            ("humidity_percentage", df.filter((col("humidity") >= 0) & (col("humidity") <= 100)).count()),
+            ("clouds_all_percentage", df.filter((col("clouds_all") >= 0) & (col("clouds_all") <= 100)).count()),
         ])
         logger.info(f"Resultados de calidad (weather): {spark.createDataFrame([Row(**quality_checks)])}")
         report_data.update(quality_checks)
@@ -105,8 +104,12 @@ if __name__ == '__main__':
     valid_weather_schema = validate_weather_schema(weather_df)
     if valid_weather_schema:
         weather_quality_checks = count_duplicates(weather_df, ["dt_iso", "city_name"], weather_quality_checks)
-        weather_quality_checks = count_nulls(weather_df, weather_quality_checks)
+        logger.info(f"1. Weather: {spark.createDataFrame([Row(**weather_quality_checks)])}")
+        weather_quality_checks = count_nulls(weather_df, weather_quality_checks, ['dt_iso','city_name','temp','temp_min','temp_max','pressure','humidity','wind_speed','wind_deg','rain_1h','rain_3h','snow_3h','clouds_all','weather_main','weather_description'])
+        logger.info(f"2. Weather: {spark.createDataFrame([Row(**weather_quality_checks)])}")
         weather_quality_checks = weather_quality_rules(weather_df, weather_quality_checks)
+        logger.info(f"3. Weather: {spark.createDataFrame([Row(**weather_quality_checks)])}")
+
         weather_quality_checks.update({'total_rows': weather_df.count()})
         weather_quality_checks_df = spark.createDataFrame([Row(**OrderedDict(weather_quality_checks))])
         weather_quality_checks_df.coalesce(1).write.csv(f"gs://{gcs_bucket_name}/reports/weather_quality_checks.csv", header=True, mode="overwrite")
