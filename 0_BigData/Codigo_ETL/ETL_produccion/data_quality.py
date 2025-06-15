94a4582dac0710faa01d6411ad1a98f2f344b460
inesposes <inesposes@gmail.com>
2025-06-15 09:27:50 +0200

    Correccións viernes

--- 0_BigData/Codigo_ETL/ETL_produccion/data_quality.py
+++ 0_BigData/Codigo_ETL/ETL_produccion/data_quality.py
@@ -3,59 +3,10 @@ from pyspark.sql.functions import col, avg, sum as spark_sum
 from logger_config import logger
 from pyspark.sql.types import *
 from collections import OrderedDict
+from excel_update import generate_data_report
+from data_utils import get_data, validate_energy_schema, validate_weather_schema
 
-def validate_weather_schema(df):
-    expected_columns = [
-        "dt_iso", "city_name", "temp", "temp_min", "temp_max", "pressure", "humidity",
-        "wind_speed", "wind_deg", "rain_1h", "rain_3h", "snow_3h", "clouds_all",
-        "weather_id", "weather_main", "weather_description", "weather_icon"
-    ]
-    if df.columns == expected_columns:
-        return True
-    else:
-        missing = [col for col in expected_columns if col not in df.columns]
-        extra = [col for col in df.columns if col not in expected_columns]
-        logger.info('Invalid weather schema')
-        return False
 
-def validate_energy_schema(df):
-    fixed_columns = ["time","forecast solar day ahead","forecast wind offshore eday ahead", 
-                     "forecast wind onshore day ahead","total load forecast",
-                     "total load actual", "price day ahead","price actual"]
-    
-    generation_prefix = "generation "
-
-    columns = df.columns
-    missing_fixed = [col for col in fixed_columns if col not in columns]
-    if missing_fixed:
-        logger.info('Invalid energy schema')
-        return False
-
-    generation_columns = [col for col in columns if col.startswith(generation_prefix)]
-
-    if len(generation_columns) == 0:
-        logger.info('Invalid energy schema')
-        return False
-
-    allowed_columns = set(fixed_columns) | set(generation_columns)
-    extra_columns = [col for col in columns if col not in allowed_columns]
-    if extra_columns:
-        logger.info('Invalid energy schema')
-        return False
-
-    return True
-
-def get_data(spark, input_path):
-    logger.info(f"Leyendo datos desde: {input_path}")
-    try:
-        df = spark.read.csv(input_path, header=True, inferSchema=True)
-        logger.info(f"Total de registros leídos: {df.count()}")
-        schema_string = df.schema.simpleString()
-        logger.info(f"\n{schema_string}")
-        return df
-    except Exception as e:
-        logger.error(f"Error al leer los datos desde {input_path}: {e}")
-        return None
 
 def count_duplicates(df, target_columns, report_data):
     if not df:
@@ -172,6 +123,8 @@ if __name__ == '__main__':
         energy_quality_checks.update({'total_rows': energy_df.count()})
         energy_quality_quality_checks_df = spark.createDataFrame([Row(**OrderedDict(energy_quality_checks))])
         energy_quality_quality_checks_df.coalesce(1).write.csv(f"gs://{gcs_bucket_name}/reports/energy_quality_checks.csv", header=True, mode="overwrite")
+    
+    generate_data_report(gcs_bucket_name)
 
     logger.info("El script de informe de calidad de datos ha finalizado.")
     spark.stop()
